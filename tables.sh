684cc44174602197b1f7b90ad3b1ad00082882f1
PyHackoras <31069505+PyHackoras@users.noreply.github.com>
2017-08-18 11:50:30 +0100

    Create tables.sh

---
+++ tables.sh
@@ -0,0 +1,5 @@
+#sort out iptables
+
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE  
+sudo iptables -A FORWARD -i eth0 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT  
+sudo iptables -A FORWARD -i wlan1 -o eth0 -j ACCEPT 
